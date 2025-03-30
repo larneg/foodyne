c7909b1bca891f2288d4abe09a84506d46ac9b02
larneg <larneg123@gmail.com>
2025-03-30 20:20:58 +0330

    Delete src directory

--- src/app.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll(".accordion-btn").forEach(button => {
-    button.addEventListener("click", () => {
-        const panel = button.nextElementSibling;
-        panel.style.display = panel.style.display === "block" ? "none" : "block";
-    });
-});
