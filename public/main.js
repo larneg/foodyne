45ca52e0cb08ed2e0949ce885ada5727718989e7
larneg <larneg123@gmail.com>
2025-04-01 12:00:35 +0330

    Add files via upload
    2.3

---
+++ public/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById('root').innerHTML = `
+  <div style="padding: 2rem; font-size: 1.5rem;">
+    <h1>🍔 Foodyne v2.3</h1>
+    <p>Welcome to the smarter way of managing restaurant orders!</p>
+  </div>
+`;
